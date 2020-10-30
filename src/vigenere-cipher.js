bcd1c0614f956326d97cbb90d7e60da641bf76cb
Evgeny <67653783+Latvels@users.noreply.github.com>
2020-10-30 20:28:17 +0300

    Update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,59 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(modification){
+        this.modification = modification;
+    }
+
+    encrypt(message, key) {
+        if (!message || !key) throw new Error();
+        let result = '';
+        let messageStr = message.toUpperCase();
+        let keywordStr = key.toUpperCase();
+        let char = 0;
+
+        for (let i = 0; i < messageStr.length; i++) {
+            if (messageStr.charCodeAt(i) > 64 && messageStr.charCodeAt(i) < 91) {   
+                if (char >= keywordStr.length) {
+                    char = 0;
+                }                    
+                    result += String.fromCharCode(((messageStr.charCodeAt(i) + keywordStr.charCodeAt(char++)) % 26) + 65);  
+            }
+            else {
+                result += messageStr[i];
+            } 
+
+            if (this.modification === false) {
+                return result.split('').reverse().join('');
+            }
+        }
+        return result;
+    }
+
+    decrypt(encryptedMessage, key) {
+        if (!encryptedMessage || !key) throw new Error();
+        let result = '';
+        let encryptedStr = encryptedMessage.toUpperCase();
+        let keywordStr = key.toUpperCase();
+        let char = 0;
+
+        for (let i = 0; i < encryptedStr.length; i++) {
+            if (encryptedStr.charCodeAt(i) > 64 && encryptedStr.charCodeAt(i) < 91) {   
+                if (char >= keywordStr.length) {
+                    char = 0;
+                }
+                result += String.fromCharCode(((encryptedStr.charCodeAt(i) + 26 - keywordStr.charCodeAt(char++)) % 26) + 65);                       
+            }
+            else {
+                result += encryptedStr[i];
+            }   
+        }
+
+        if (this.modification === false) {
+            return result.split('').reverse().join('');
+        }
+        return result;
+    }
 }
 
 module.exports = VigenereCipheringMachine;
