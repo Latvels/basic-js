daf8d3112130f77c8d558f7bd848779e05308f32
Evgeny <67653783+Latvels@users.noreply.github.com>
2020-10-30 15:33:45 +0300

    Update hanoi-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,4 @@
-
+const CustomError = require("../extensions/custom-error");
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
   let turns = Math.pow(2, disksNumber) - 1;
   let sec = turnsSpeed / 3600;
